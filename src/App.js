481462e45681296dd8fa733e32551efa2e2f75fc
PalmTreeBlvd <55242169+PalmTreeBlvd@users.noreply.github.com>
2020-09-02 11:27:09 -0400

    tweets now center and stack on one another

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from "react";
 import MainFeed from "./components/mainFeed/MainFeed";
 // as of now, this class is not using App.css
-import "./App.css";
+//import "./App.css";
 
 // To visualize CSS in browser, in Chrome inspector go to arrow, components, and hover mouse over classes and their properties
 export default function App() {
   return (
-    <div style={tweetContainter}>
-      <MainFeed></MainFeed>
+    <div style={tweetContainterCenterWidth}>
+      <div>
+        <MainFeed></MainFeed>
+        <MainFeed></MainFeed>
+      </div>
     </div>
   );
 }
 
-const tweetContainter = {
-  // these two elements make tweets appear at the center of the class
+// Div using this container centers tweets on screen.
+// A second div is then used to make objects appear on top of one another.
+const tweetContainterCenterWidth = {
+  // makes row left to right
   display: "flex",
+  // centers items on row
   justifyContent: "center",
+};
+
+const upAndDown = {
+  backgroundColor: "blue",
+  height: "200px",
 };
--- src/components/mainFeed/MainFeed.js
+++ src/components/mainFeed/MainFeed.js
@@ -3,7 +3,7 @@ import React from "react";
 // By making them both lists <li>, instead of items <ul> in a list, they go from left to right
 export default function MainFeed() {
   return (
-    <div style={row}>
+    <div style={headContainer}>
       <div style={name}>Peter Parker</div>
       <div style={name}>@peterparker89</div>
       <div style={name}>*</div>
@@ -12,7 +12,7 @@ export default function MainFeed() {
   );
 }
 
-const row = {
+const headContainer = {
   // How wide row is
   width: "300px",
   // makes grid from left to right
