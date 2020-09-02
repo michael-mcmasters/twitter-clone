8bea3b755a32ae53c9d911efd338f209be95f4c4
PalmTreeBlvd <55242169+PalmTreeBlvd@users.noreply.github.com>
2020-09-02 11:03:39 -0400

    using CSS, tweets are now centered on screen. they have a name, a userName, and time since last tweet. All in plain text for now.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import MainFeed from "./components/mainFeed/MainFeed";
+// as of now, this class is not using App.css
 import "./App.css";
 
-function App() {
+// To visualize CSS in browser, in Chrome inspector go to arrow, components, and hover mouse over classes and their properties
+export default function App() {
   return (
-    <div className="app">
+    <div style={tweetContainter}>
       <MainFeed></MainFeed>
     </div>
   );
 }
 
-export default App;
+const tweetContainter = {
+  // these two elements make tweets appear at the center of the class
+  display: "flex",
+  justifyContent: "center",
+};
--- src/components/mainFeed/MainFeed.js
+++ src/components/mainFeed/MainFeed.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-function MainFeed() {
-  return <h1>Hello World!</h1>;
+// By making them both lists <li>, instead of items <ul> in a list, they go from left to right
+export default function MainFeed() {
+  return (
+    <div style={row}>
+      <div style={name}>Peter Parker</div>
+      <div style={name}>@peterparker89</div>
+      <div style={name}>*</div>
+      <div style={name}>16h</div>
+    </div>
+  );
 }
 
-export default MainFeed;
+const row = {
+  // How wide row is
+  width: "300px",
+  // makes grid from left to right
+  display: "flex",
+  // space between text from edge of screen
+  margin: "10px",
+};
+
+const name = {
+  // space between word on left
+  paddingLeft: "10px",
+};
