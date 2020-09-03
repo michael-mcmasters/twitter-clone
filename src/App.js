ee6585a4f0e540ccb82caa2cd44609a277dc7275
PalmTreeBlvd <55242169+PalmTreeBlvd@users.noreply.github.com>
2020-09-03 16:15:09 -0400

    Re-did how html in App.js works so I can more easily add the NavBar and any components on the right side

--- src/App.js
+++ src/App.js
@@ -8,36 +8,70 @@ import { ColorThemeProvider } from "./components/customHooks/ColorThemeContext.j
 
 // To visualize CSS in browser, in Chrome inspector go to arrow, components, and hover mouse over classes and their properties
 // first <div style> puts tweets category in row. second <div> puts individual tweets in a column
+// export default function App() {
+//   return (
+//     <React.Fragment>
+//       <div style={{ border: "3px solid #38444d" }}>
+//         <NavBar></NavBar>
+//       </div>
+//       <div style={tweetsContainer}>
+//         <ColorThemeProvider>
+//           <ColorThemeToggle></ColorThemeToggle>
+//           <div>
+//             <Tweet style={upAndDown}></Tweet>
+//             <Tweet></Tweet>
+//             <Tweet></Tweet>
+//             <Tweet></Tweet>
+//           </div>
+//         </ColorThemeProvider>
+//       </div>
+//     </React.Fragment>
+//   );
+// }
+
+// // Div using this container centers tweets on screen.
+// // A second div is then used to make objects appear on top of one another.
+// const tweetsContainer = {
+//   // makes row left to right
+//   display: "flex",
+//   // centers items on row
+//   justifyContent: "center",
+// };
+
+// const upAndDown = {
+//   backgroundColor: "blue",
+//   border: "3px blue",
+//   minHeight: "200px",
+// };
+
 export default function App() {
   return (
     <React.Fragment>
-      <NavBar></NavBar>
-      <div style={tweetsContainer}>
-        <ColorThemeProvider>
+      <ColorThemeProvider>
+        <div style={row}>
+          {/* Left row */}
+          <div>First</div>
+          <div>Second</div>
           <ColorThemeToggle></ColorThemeToggle>
           <div>
-            <Tweet style={upAndDown}></Tweet>
+            {/* Center row ... Items here go in column up and down */}
+            <Tweet></Tweet>
             <Tweet></Tweet>
             <Tweet></Tweet>
             <Tweet></Tweet>
           </div>
-        </ColorThemeProvider>
-      </div>
+          {/* Right row */}
+          <div>Third</div>
+          <div>Fourth</div>
+        </div>
+      </ColorThemeProvider>
     </React.Fragment>
   );
 }
 
-// Div using this container centers tweets on screen.
-// A second div is then used to make objects appear on top of one another.
-const tweetsContainer = {
-  // makes row left to right
+const row = {
   display: "flex",
-  // centers items on row
   justifyContent: "center",
 };
 
-const upAndDown = {
-  backgroundColor: "blue",
-  border: "3px blue",
-  minHeight: "200px",
-};
+const tweetContainer = {};
