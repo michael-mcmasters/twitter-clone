b7e7e7024ad09cd5f3bfcd3b6c66ec35e3611017
PalmTreeBlvd <55242169+PalmTreeBlvd@users.noreply.github.com>
2020-09-07 15:22:52 -0400

    Created Program.js. App.js holds contexts and Program.js is a child that holds all other child components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
 import React from "react";
-//import "./App.css";
-import Tweets from "./components/tweets/Tweets";
-//import NavBar from "./components/navBar/NavBar";
-import ColorThemeToggle from "./components/ColorThemeToggle";
+import Program from "./components/Program";
 import { ColorThemeProvider } from "./components/customHooks/ColorThemeContext.js";
-import Background from "./components/background/Background.js";
 
+// Program.js is a child component of App.js and holds all other child components.
+// App.js holds global contexts that program.js needs such as the background color.
 export default function App() {
-  // ToDo: You can use position: fixed for the sides and they will stay in position no matter what.
-  // So that when we scroll, the left navbar and the right side won't scroll. Only the tweets will.
   return (
     <ColorThemeProvider>
-      <React.Fragment>
-        <div style={background(colorTheme)}>
-          <div style={row}>
-            {/* Left row */}
-            <div>First</div>
-            <ColorThemeToggle></ColorThemeToggle>
-            <div>Second</div>
-            <div>
-              {/* Center row ... Items here go in column up and down */}
-              <Tweets></Tweets>
-              ))}
-            </div>
-            {/* Right row */}
-            <div>Third</div>
-            <div>Fourth</div>
-          </div>
-        </div>
-      </React.Fragment>
+      <Program></Program>
     </ColorThemeProvider>
   );
 }
-
-function background(colorTheme) {
-  return {
-    backgroundColor: colorTheme.background,
-  };
-}
-
-const row = {
-  // forces row to go left to right instead of up and down
-  display: "flex",
-  // centers items on row
-  justifyContent: "center",
-};
---
+++ src/components/Program.js
@@ -0,0 +1,43 @@
+import React from "react";
+//import "./App.css";
+import Tweets from "./tweets/Tweets";
+import ColorThemeToggle from "./ColorThemeToggle";
+//import NavBar from "./components/navBar/NavBar";
+
+export default function Program() {
+  // ToDo: You can use position: fixed for the sides and they will stay in position no matter what.
+  // So that when we scroll, the left navbar and the right side won't scroll. Only the tweets will.
+  return (
+    <React.Fragment>
+      <div>
+        <div style={row}>
+          {/* Left row */}
+          <div>First</div>
+          <ColorThemeToggle></ColorThemeToggle>
+          <div>Second</div>
+          <div>
+            {/* Center row ... Items here go in column up and down */}
+            <Tweets></Tweets>
+            ))
+          </div>
+          {/* Right row */}
+          <div>Third</div>
+          <div>Fourth</div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function background(colorTheme) {
+  return {
+    backgroundColor: colorTheme.background,
+  };
+}
+
+const row = {
+  // forces row to go left to right instead of up and down
+  display: "flex",
+  // centers items on row
+  justifyContent: "center",
+};
